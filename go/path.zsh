a104ab7fe74892dc300b09069ec845fad85bf235
Khang Toh <khang.toh@gmail.com>
2019-11-28 13:20:10 +0800

    Added flutter and update zshrc

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,3 @@
 export GOPATH=/Data/working_copies/go-workspace
 export PATH=$PATH:$GOPATH/bin
+export PATH="$PATH:/usr/local/go"
--- system/env.zsh
+++ system/env.zsh
@@ -2,12 +2,5 @@ export LANG=en_US.UTF-8
 export EDITOR=/usr/bin/vim
 export GREP_OPTIONS='--color=auto'
 
-# Nitrous.IO VM.
-export DEV_COOKBOOK_VM_CPUS=2
-export DEV_COOKBOOK_VM_MEM=3072
-export DEV_COOKBOOK_SYNC_WORKSPACE=1
-export DEV_COOKBOOK_WORKSPACE=/Data/working_copies/nitrous-workspace
-export BOMBAY_INTERFACE=33.33.33.12
-
 # Docker.
 export DOCKER_HOST=tcp://localhost:4243
